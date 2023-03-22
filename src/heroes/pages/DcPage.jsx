b92cf02b8422814a07d5807bc578db8ab0bc7fa5
Paulespinozarl <paulespinozarl@gmail.com>
2023-03-22 19:01:20 -0300

    Mi primer commit

---
+++ src/heroes/pages/DcPage.jsx
@@ -0,0 +1,12 @@
+import { HeroList } from "../components";
+
+export const DcPage = () => {
+  return (
+    <><h1 className="mt-3">Dc Comics</h1>
+    <hr />
+
+    <HeroList publisher='DC Comics'/>
+    
+    </>
+  )
+};
